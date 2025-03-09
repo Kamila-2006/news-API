af184c173ab711177799ca934a2e0dc45fda21e5
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-03-09 19:32:52 +0500

    new model created

--- news/models.py
+++ news/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from categories.models import Category
+from tags.models import Tag
 
-# Create your models here.
+
+class New(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='news')
+    tags = models.ManyToManyField(Tag, related_name='news')
+    image = models.ImageField(required=False)
+    views_count = models.PositiveIntegerField(default=0)
+    is_published = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
