0c58965696957486ba1d2f7b9efeb88c59def950
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-03-14 16:01:19 +0500

    views_count created

--- news/views.py
+++ news/views.py
@@ -9,3 +9,9 @@ class NewViewSet(viewsets.ModelViewSet):
     serializer_class = NewSerializer
     pagination_class = NewPagination
     lookup_field = 'slug'
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.views_count += 1
+        instance.save(update_fields=['views_count'])
+        return super().retrieve(request, *args, **kwargs)
